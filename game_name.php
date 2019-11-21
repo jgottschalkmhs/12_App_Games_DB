001799d845e396649a59630268b93f8740ea0ddd
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2019-11-21 22:29:55 +1300

    Update game_name.php

--- game_name.php
+++ game_name.php
@@ -52,12 +52,33 @@ if(isset($_POST['find_game_name']))
             <!-- Results go here -->
             <div class="results">
             
-            <p>Game Name: <span class="sub_heading"><?php echo $find_rs['Name'];?></span></p>
+            <p>
+                Game Name: 
+                <span class="sub_heading">
+                
+                <a target="_blank" href="<?php echo $find_rs['URL']; ?>">
+                    <?php echo $find_rs['Name'];?></a>
+                
+                </span>
+                
+                </p>
                 
             <p>Developer: <?php echo $find_rs['DevName'];?></p>
                 
             
             <p>Genre: <?php echo $find_rs['GenreName'];?></p>
+                
+                <hr />
+                
+            <p>
+                <span class="sub_heading">Description</span>
+            </p>
+            
+            
+                
+            <p>
+            <?php echo $find_rs['Description']; ?>    
+            </p>
 
             
             <br />
