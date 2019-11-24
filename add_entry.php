f43a509624398b4c83f9c32bccc05e1ea2fb5bb9
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2019-11-25 09:54:31 +1300

    Drop downs and styling
    Have sort of working genre drop down and developer boxes.  Need to make these into combo boxes

--- add_entry.php
+++ add_entry.php
@@ -1,6 +1,15 @@
 <?php 
     include "topbit.php"; 
     
+
+    // sql to populate genre field
+  $genre_sql="SELECT * FROM `genre` ORDER BY `genre`.`GenreName` ASC 
+    " ;
+    $genre_query=mysqli_query($dbconnect, $genre_sql);
+    $genre_rs=mysqli_fetch_assoc($genre_query);
+    $count=mysqli_num_rows($genre_query);
+
+
     
     // Initialise variables
 
@@ -9,6 +18,8 @@
     $app_name = "";
     $url = "";
     $subtitle = "";
+    $genre = "";
+    $developer = "";
 
     // Error Checking
     
@@ -42,6 +53,45 @@
 			<input type="text" name="url" size="45" value="<?php echo $url; ?>" required  placeholder="http://" />
 		</p>
                
+        <div class="flex-container">
+            
+            <div class="genere">
+            <b>Genre:</b><br />
+			<select name="genre">
+                   <option value="" disabled selected>Genre...</option>
+                   
+                   <!-- options from database -->
+                   
+                   <?php
+                   
+                   do{ ?>
+                    <option value="<?php echo $genre_rs['GenreName'];?>"><?php echo $genre_rs['GenreName'];?></option>
+                   
+                   
+                   <?php
+                       
+                   } // end do
+                   
+                   while($genre_rs=mysqli_fetch_assoc($genre_query))
+                       
+                   ?>
+                   
+               </select>
+            </div> <!-- / genre div -->
+            
+            <!-- nbsp's below add small gap between drop down boxes -->
+            &nbsp; &nbsp; &nbsp;
+            
+            <div class="developer">
+            <b>Developer:</b><br />
+			<select name="developer">
+				<option value="companyA">companyA</option>
+				<option value="card">companyB</option>
+				<option value="adventure">companyC</option>
+			</select>
+            </div> <!-- / developer div -->
+            
+        </div>  <!-- / flexbox -->
         
             </form>
 
