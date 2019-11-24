8e86c6375cadd21a2138e95ed747fdee3557b229
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2019-11-25 07:58:37 +1300

    Update allentries.php

--- allentries.php
+++ allentries.php
@@ -17,58 +17,9 @@
         <div class="box main">
             <h2>All Results</h2>
             
-            <?php
-            
-            if ($count<1)
-                
-            {
-                ?>
-            
-            <div class="error">
-            
-                Sorry!  There are no results that match your search.  Please use the search box in the side bar to try again.
-                
-            </div>  <!-- end error -->
-            
-            <?php 
-            }
-            
-            else
-                
-            {
-                do {
-                    
-                    ?>
-            
-            <!-- Results go here -->
-            <div class="results">
-            
-            <p>Game Name: <span class="sub_heading"><?php echo $find_rs['Name'];?></span></p>
-                
-            <p>Developer: <?php echo $find_rs['DevName'];?></p>
-                
-            
-            <p>Genre: <?php echo $find_rs['GenreName'];?></p>
-
-            
-            <br />
-                
-           </div>  <!-- / results -->
-            <br />
-            
-            
-            <?php
-                }
-                
-                while
-                    ($find_rs=mysqli_fetch_assoc($find_query));
-                
-            }
-            
-            
+           <?php include("results.php");
             
             ?>
-        
  
           
             
